2561d7813bc358a72447d1191f948139cc140ffc
Tino Junge <tino.junge@futurice.com>
2016-04-14 19:33:20 +0100

    Add env.sample file

---
+++ env.example.js
@@ -0,0 +1,8 @@
+// Secrets for the applications. Do NOT commit any secrets to version control.
+// 1. cp env.example.js env.js
+// 2. Fill in the blanks
+
+module.exports = {
+  AUTH0_CLIENT_ID: '',
+  AUTH0_NAMESPACE: ''
+};
