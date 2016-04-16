a54fb6e4f1764d79314456d7a6a4e3304865c7c7
Jani Eväkallio <jani.evakallio@gmail.com>
2016-04-16 16:50:21 +0100

    :fire: Use redux-loop for side effect and immutable data management

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,18 +1,29 @@
-import {combineReducers} from 'redux-immutablejs';
+import {Map} from 'immutable';
+import {combineReducers} from 'redux-loop';
 import AppStateUpdater from '../state/AppState';
 import CounterStateUpdater from '../state/counter/CounterState';
 
-const namespacedReducer = combineReducers({
+const reducers = {
   counter: CounterStateUpdater
-});
+};
+
+// initial state, accessor and mutator for supporting root-level
+// immutable data with redux-loop reducer combinator
+const immutableStateContainer = Map();
+const getImmutable = (child, key) => child ? child.get(key) : void 0;
+const setImmutable = (child, key, value) => child.set(key, value);
+
+const namespacedReducer = combineReducers(
+  reducers,
+  immutableStateContainer,
+  getImmutable,
+  setImmutable
+);
 
 export default function mainReducer(state, action) {
   // on init, gather initial states from all reducers
-  if (action.type === '@@redux/INIT') {
-    /*eslint-disable no-undefined*/
-    const initialState = undefined;
-    /*eslint-enable no-undefined*/
-    return AppStateUpdater(initialState, action).merge(namespacedReducer(initialState, action));
+  if (action.type === '@@ReduxLoop/INIT') {
+    return AppStateUpdater(state, action).merge(namespacedReducer(state, action));
   }
 
   // on subsequent actions, allow AppStateUpdater to do global state update
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,19 +4,16 @@ import * as reduxLoop from 'redux-loop';
 import middleware from './middleware';
 import reducer from './reducer';
 
-// compose redux store enhancers here
-const installEnhancers = compose(
-  reduxLoop.install
+const enhancer = compose(
+  applyMiddleware(...middleware),
+  reduxLoop.install()
 );
 
-// register middlewares defined in the redux/middleware module
-const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
-
 // create the store
-const store = createStoreWithMiddleware(
+const store = createStore(
   reducer,
   null,
-  installEnhancers()
+  enhancer
 );
 
 export default store;
