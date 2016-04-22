f4304a7bb9c711b86a71cc7f2efc252f94721535
Tino Junge <tino.junge@futurice.com>
2016-04-22 13:09:10 +0100

    Setup android app. Include es6-symbol lib to handle Symbol error on android

--- index.android.js
+++ index.android.js
@@ -1,14 +1,20 @@
+import 'es6-symbol/implement';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
-import CounterViewContainer from './src/views/CounterViewContainer';
+import AppView from './src/modules/AppView';
 
 import React from 'react-native';
+import * as auth0 from './src/services/auth0';
 
 const Kindling = React.createClass({
+  componentDidMount() {
+    // auth0.showLogin();
+  },
+
   render() {
     return (
       <Provider store={store}>
-        <CounterViewContainer />
+        <AppView />
       </Provider>
     );
   }
