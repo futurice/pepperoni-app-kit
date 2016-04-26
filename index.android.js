627104bf24d24577899f3bdafcaaf3543281bd19
Jani Eväkallio <jani.evakallio@gmail.com>
2016-04-26 10:53:51 +0100

    Add support to Android back button

--- index.android.js
+++ index.android.js
@@ -2,13 +2,28 @@ import 'es6-symbol/implement';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
 import AppView from './src/modules/AppView';
-
-import React from 'react-native';
+import React, {BackAndroid} from 'react-native';
+import * as NavigationStateActions from './src/modules/navigation/NavigationState';
 import * as auth0 from './src/services/auth0';
 
 const Kindling = React.createClass({
+
+  componentWillMount() {
+    BackAndroid.addEventListener('hardwareBackPress', this.navigateBack);
+  },
+
   componentDidMount() {
     auth0.showLogin();
+  },
+
+  navigateBack() {
+    const navigationState = store.getState().get('navigationState');
+    if (navigationState.index === 0) {
+      return false;
+    }
+
+    store.dispatch(NavigationStateActions.popRoute());
+    return true;
   },
 
   render() {
