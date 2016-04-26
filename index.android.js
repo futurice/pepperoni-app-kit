b969a33cf4616bce1138a23946e7e5506f12d1ee
Jani Eväkallio <jani.evakallio@gmail.com>
2016-04-26 16:35:50 +0100

    Go back to home tab when exhausted other tab

--- index.android.js
+++ index.android.js
@@ -19,7 +19,17 @@ const Kindling = React.createClass({
   navigateBack() {
     const navigationState = store.getState().get('navigationState');
     const currentTab = navigationState.children[navigationState.index];
+
+    // if we are in the beginning of our tab stack
     if (currentTab.index === 0) {
+
+      // if we are not in the first tab, switch tab to the leftmost one
+      if (navigationState.index !== 0) {
+        store.dispatch(NavigationStateActions.switchTab(0));
+        return true;
+      }
+
+      // otherwise let OS handle the back button action
       return false;
     }
 
