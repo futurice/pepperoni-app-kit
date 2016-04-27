105eb430da27bcef57990e06753dff21723df0fe
Jani Eväkallio <jani.evakallio@gmail.com>
2016-04-27 12:12:21 +0100

    Move app core logic to AppView, show loading spinner

--- index.ios.js
+++ index.ios.js
@@ -1,37 +1,15 @@
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
-import AppView from './src/modules/AppView';
+import AppViewContainer from './src/modules/AppViewContainer';
 
 import React from 'react-native';
-import * as auth0 from './src/services/auth0';
-import {saveSnapshot, resetSnapshot} from './src/utils/snapshot';
 
 const Kindling = React.createClass({
-  componentDidMount() {
-
-    resetSnapshot()
-      .then(snapshot => {
-        if (snapshot) {
-          store.dispatch({
-            type: '@@snapshot/reset',
-            payload: snapshot
-          });
-        }
-
-        if (!snapshot || !snapshot.getIn(['auth', 'isLoggedIn'])) {
-          auth0.showLogin();
-        }
-
-        store.subscribe(() => {
-          saveSnapshot(store.getState());
-        });
-      });
-  },
 
   render() {
     return (
       <Provider store={store}>
-        <AppView />
+        <AppViewContainer />
       </Provider>
     );
   }
