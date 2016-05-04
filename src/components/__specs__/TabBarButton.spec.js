85276a89250f82a7fb7157098af8bbb6245318e8
Jani Eväkallio <jani.evakallio@gmail.com>
2016-05-04 17:48:27 +0100

    Sample tests for TabBar

---
+++ src/components/__specs__/TabBarButton.spec.js
@@ -0,0 +1,46 @@
+/*eslint-disable max-nested-callbacks*/
+
+import sinon from 'sinon';
+import React, {TouchableOpacity, Text} from 'react-native';
+import {shallow} from 'enzyme';
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import {hasStyles} from '../../../test/assertions';
+
+import TabBarButton from '../TabBarButton';
+
+describe('<TabBarButton/>', () => {
+
+  it('should render the text property as label', () => {
+    const wrapper = shallow(
+      <TabBarButton text='TestButton' isSelected={true} action={() => null} />
+    );
+
+    expect(wrapper.contains(<Text>TestButton</Text>)).to.equal(true);
+  });
+
+  it('should respond to press events', () => {
+    const onPress = sinon.spy();
+    const wrapper = shallow(
+      <TabBarButton text='TestButton' action={onPress} isSelected={false} />
+    );
+
+    expect(onPress.called).to.equal(false);
+
+    wrapper.find(TouchableOpacity).simulate('press');
+    expect(onPress.calledOnce).to.equal(true);
+  });
+
+  it('should render selected button with a background color', () => {
+    const unselected = shallow(
+      <TabBarButton text='TestButton' action={() => null} isSelected={false} />
+    );
+
+    const selected = shallow(
+      <TabBarButton text='TestButton' action={() => null} isSelected={true} />
+    );
+
+    expect(hasStyles(unselected.first(), {backgroundColor: 'yellow'})).to.equal(false);
+    expect(hasStyles(selected.first(), {backgroundColor: 'yellow'})).to.equal(true);
+  });
+});
