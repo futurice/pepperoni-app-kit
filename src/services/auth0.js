3865fa6fe324c183654dc79607464582b1f4ddd1
Jani Eväkallio <jani.evakallio@gmail.com>
2016-05-05 18:37:22 +0100

    Don't show auth dialog if keys aren't configured

--- src/services/auth0.js
+++ src/services/auth0.js
@@ -1,18 +1,30 @@
-import * as ENV from '../../env';
+import * as env from '../../env';
 import Auth0Lock from 'react-native-lock';
 import * as AuthStateActions from '../modules/auth/AuthState';
 import store from '../redux/store';
 
-const lock = new Auth0Lock({
-  clientId: ENV.AUTH0_CLIENT_ID,
-  domain: ENV.AUTH0_NAMESPACE
-});
+const clientId = env.AUTH0_CLIENT_ID;
+const domain = env.AUTH0_NAMESPACE;
+const authenticationEnabled = clientId && domain;
+
+let lock = null;
+if (authenticationEnabled) {
+  lock = new Auth0Lock({
+    clientId,
+    domain
+  });
+} else {
+  console.warn('Authentication not enabled: Auth0 configuration not provided');
+}
 
 export function showLogin() {
+  if (!authenticationEnabled) {
+    return;
+  }
+
   const options = {
     closable: true
   };
-
   lock.show(options, (err, profile, token) => {
     if (err) {
       store.dispatch(AuthStateActions.onUserLoginError(err));
