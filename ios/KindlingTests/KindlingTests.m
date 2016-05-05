d393d390ea6df7160f9aa2b0b484728c974039ce
Jani Eväkallio <jani.evakallio@gmail.com>
2016-05-05 18:16:16 +0100

    Fix iOS tests

--- ios/KindlingTests/KindlingTests.m
+++ ios/KindlingTests/KindlingTests.m
@@ -14,7 +14,7 @@
 #import "RCTRootView.h"
 
 #define TIMEOUT_SECONDS 240
-#define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
+#define TEXT_TO_LOOK_FOR @"I'm bored!"
 
 @interface KindlingTests : XCTestCase
 
