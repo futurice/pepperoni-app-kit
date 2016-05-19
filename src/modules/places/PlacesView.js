b5ce366ae1532780c61cdfd74119914ee4419868
Tino Junge <tino.junge@futurice.com>
2016-05-19 17:45:42 +0100

    Add a MapView for places, iOS only

--- src/modules/places/PlacesView.js
+++ src/modules/places/PlacesView.js
@@ -2,12 +2,11 @@ import React, {PropTypes} from 'react';
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  MapView
 } from 'react-native';
 
 import * as NavigationState from '../../modules/navigation/NavigationState';
-
-const color = () => Math.floor(255 * Math.random());
 
 const placesData = require('../../data/samplePlaces.json');
 const places = placesData.London;
@@ -22,7 +21,7 @@ const PlacesView = React.createClass({
 
   getInitialState() {
     return {
-      background: `rgba(${color()},${color()},${color()}, 1)`
+      backgroundColor: 'white'
     };
   },
 
@@ -37,6 +36,20 @@ const PlacesView = React.createClass({
   render() {
 
     const place = places[randomPicker()];
+    const marker = [
+      {
+        latitude: place.latitude,
+        longitude: place.longitude,
+        title: place.name,
+        subtitle: place.type
+      }
+    ];
+    const region = {
+      latitude: place.latitude,
+      longitude: place.longitude,
+      latitudeDelta: 0.005,
+      longitudeDelta: 0.005
+    };
 
     return (
       <View style={[styles.container, {backgroundColor: this.state.background}]}>
@@ -49,9 +62,11 @@ const PlacesView = React.createClass({
         <Text style={styles.placeInfo}>
           Distance: {place.distance}
         </Text>
-        <Text style={styles.placeInfo}>
-          Address: {place.address}
-        </Text>
+        <MapView
+          style={styles.map}
+          region={region}
+          annotations={marker}
+        />
       </View>
     );
   }
@@ -68,6 +83,16 @@ const styles = StyleSheet.create({
   },
   placeInfo: {
     fontSize: 20
+  },
+  placeAddress: {
+    fontSize: 15
+  },
+  map: {
+    height: 150,
+    width: 250,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: '#000000'
   }
 });
 
