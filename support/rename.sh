4fa044db7cb249568044ef957b963f3f32d2a86d
chadrien <chadrien@chadrien.fr>
2016-05-28 21:37:25 +0200

    Fix path now that the scipt is in a subdirectory

--- support/rename.sh
+++ support/rename.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 newAppName=$1
+appRoot=`dirname $0`/..
 
 [ -z "${newAppName}" ] && echo 'Missing required parameter newAppName' && exit 1
 
-grep -rI 'PepperoniAppTemplate' --exclude='rename.sh' ./* | tr ':' ' ' | awk '{print $1}' | uniq |xargs -I{} sed -i "s/PepperoniAppTemplate/${newAppName}/g" {}
-grep -rI 'pepperoniapptemplate' --exclude='rename.sh' ./* | tr ':' ' ' | awk '{print $1}' | uniq |xargs -I{} sed -i "s/pepperoniapptemplate/`echo $newAppName | tr '[:upper:]' '[:lower:]'`/g" {}
+grep -rI 'PepperoniAppTemplate' --exclude='rename.sh' $appRoot/* | tr ':' ' ' | awk '{print $1}' | uniq |xargs -I{} sed -i "s/PepperoniAppTemplate/${newAppName}/g" {}
+grep -rI 'pepperoniapptemplate' --exclude='rename.sh' $appRoot/* | tr ':' ' ' | awk '{print $1}' | uniq |xargs -I{} sed -i "s/pepperoniapptemplate/`echo $newAppName | tr '[:upper:]' '[:lower:]'`/g" {}
 
-for fileToMove in `find ios -name '*PepperoniAppTemplate*' -depth`; do
+for fileToMove in `find $appRoot/ios -name '*PepperoniAppTemplate*' -depth`; do
   mv $fileToMove `echo $fileToMove | sed "s/\(.*\)PepperoniAppTemplate/\1$newAppName/g"`
 done
