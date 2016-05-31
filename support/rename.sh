078db03b268bc6f7eee062e4ff4de48ef0e22aba
Hadrien de Cuzey <hadrien@platinium-group.org>
2016-05-31 16:57:24 +0200

    Make the script work with vanilla osx sed

--- support/rename.sh
+++ support/rename.sh
@@ -5,9 +5,10 @@ appRoot=`dirname $0`/..
 
 [ -z "${newAppName}" ] && echo 'Missing required parameter newAppName' && exit 1
 
-grep -rI 'PepperoniAppTemplate' --exclude='rename.sh' $appRoot/* | tr ':' ' ' | awk '{print $1}' | uniq |xargs -I{} sed -i "s/PepperoniAppTemplate/${newAppName}/g" {}
-grep -rI 'pepperoniapptemplate' --exclude='rename.sh' $appRoot/* | tr ':' ' ' | awk '{print $1}' | uniq |xargs -I{} sed -i "s/pepperoniapptemplate/`echo $newAppName | tr '[:upper:]' '[:lower:]'`/g" {}
+grep -rI 'PepperoniAppTemplate' --exclude='rename.sh' $appRoot/* | tr ':' ' ' | awk '{print $1}' | uniq | xargs -I{} sed -i.bak "s/PepperoniAppTemplate/${newAppName}/g" {}
+grep -rI 'pepperoniapptemplate' --exclude='rename.sh' $appRoot/* | tr ':' ' ' | awk '{print $1}' | uniq | xargs -I{} sed -i.bak "s/pepperoniapptemplate/`echo $newAppName | tr '[:upper:]' '[:lower:]'`/g" {}
+find . -name '*.bak' -exec rm {} \;
 
-for fileToMove in `find $appRoot/ios -name '*PepperoniAppTemplate*' -depth`; do
+for fileToMove in `find $appRoot/ios -depth -name '*PepperoniAppTemplate*'`; do
   mv $fileToMove `echo $fileToMove | sed "s/\(.*\)PepperoniAppTemplate/\1$newAppName/g"`
 done
