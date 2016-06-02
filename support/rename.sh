8cb03c402a8062e7bcbfa5a0a71f38d1377a54d3
Jani Eväkallio <jani.evakallio@gmail.com>
2016-06-02 11:26:01 +0100

    Rename for lowercase android directories

--- support/rename.sh
+++ support/rename.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 newAppName=$1
+newLowerCaseName=`echo $newAppName | tr '[:upper:]' '[:lower:]'`
+
 appRoot=`dirname $0`/..
 
 [ -z "${newAppName}" ] && echo 'Missing required parameter newAppName' && exit 1
 
 grep -rI 'PepperoniAppTemplate' --exclude='rename.sh' $appRoot/* | tr ':' ' ' | awk '{print $1}' | uniq | xargs -I{} sed -i.bak "s/PepperoniAppTemplate/${newAppName}/g" {}
-grep -rI 'pepperoniapptemplate' --exclude='rename.sh' $appRoot/* | tr ':' ' ' | awk '{print $1}' | uniq | xargs -I{} sed -i.bak "s/pepperoniapptemplate/`echo $newAppName | tr '[:upper:]' '[:lower:]'`/g" {}
+grep -rI 'pepperoniapptemplate' --exclude='rename.sh' $appRoot/* | tr ':' ' ' | awk '{print $1}' | uniq | xargs -I{} sed -i.bak "s/pepperoniapptemplate/${newLowerCaseName}/g" {}
 find . -name '*.bak' -exec rm {} \;
 
 for fileToMove in `find $appRoot/ios -depth -name '*PepperoniAppTemplate*'`; do
   mv $fileToMove `echo $fileToMove | sed "s/\(.*\)PepperoniAppTemplate/\1$newAppName/g"`
+done
+
+for fileToMove in `find $appRoot/android -depth -name '*pepperoniapptemplate*'`; do
+  mv $fileToMove `echo $fileToMove | sed "s/\(.*\)pepperoniapptemplate/\1$newLowerCaseName/g"`
 done
