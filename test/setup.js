b9076562d7b5074cb61df2e895f78ac1b53abf41
Jani Viherväs <jani.vihervas@futurice.com>
2016-06-22 16:33:45 +0300

    Typo

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,5 @@
-global.__DEV__ = false;
-
 require('babel-register')({
-  // ignore node_modules except node_modules/react-native-vector-icons and node_modules/react-native-lock
+  // ignore node_modules except node_modules/react-native-gifted-spinner and node_modules/react-native-lock
   // because they need to be transpiled
   // syntax: /node_modules\/(?!(library1|library2))/
   ignore: /node_modules\/(?!(react-native-gifted-spinner|react-native-lock))/
