db919581efa8102fa67ce7644ff851f8bb0c4edb
Jani Eväkallio <jani.evakallio@gmail.com>
2016-07-01 11:13:46 +0200

    API utility

---
+++ src/utils/authentication.js
@@ -0,0 +1,15 @@
+import {AsyncStorage} from 'react-native';
+
+const AUTHENTICATION_STORAGE_KEY = 'PepperoniState:Authentication';
+
+export function getAuthenticationToken() {
+  return AsyncStorage.getItem(AUTHENTICATION_STORAGE_KEY);
+}
+
+export async function setAuthenticationToken(token) {
+  return AsyncStorage.setItem(AUTHENTICATION_STORAGE_KEY, token);
+}
+
+export async function clearAuthenticationToken() {
+  return AsyncStorage.removeItem(AUTHENTICATION_STORAGE_KEY);
+}
