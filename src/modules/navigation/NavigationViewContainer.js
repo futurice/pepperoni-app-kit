592017b942029d5fff7eb5aa828ae796e16a9c35
Jani Eväkallio <jani.evakallio@gmail.com>
2016-07-09 12:35:10 +0100

    Kill NavigationExperimental onNavigate prop as per https://github.com/facebook/react-native/commit/fb0007d85323909ab652bf97166744fa7e17daab

--- src/modules/navigation/NavigationViewContainer.js
+++ src/modules/navigation/NavigationViewContainer.js
@@ -10,17 +10,11 @@ export default connect(
     switchTab(index) {
       dispatch(switchTab(index));
     },
-    onNavigate(action) {
-      // The "back" and "BackAction" actions are fired from NavigationExperimental when
-      // the user swipes the screen back or uses the software back button, respectively.
-      // We handle these and dispatch our custom back action to our Redux stack.
-      //
-      // Android back button is handled separately in index.android.js
-      if (action.type === 'back' || action.type === 'BackAction') {
-        dispatch(popRoute());
-      } else if (action.type === 'animation-completed') {
-        dispatch(navigationCompleted());
-      }
+    onNavigateBack() {
+      dispatch(popRoute());
+    },
+    onNavigateCompleted() {
+      dispatch(navigationCompleted());
     }
   })
 )(NavigationView);
