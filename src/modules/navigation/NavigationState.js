311b3de359187b47ed344cdd1a246a3b3e4bcaec
Fabrizio Moscon <mosconfabrizio@gmail.com>
2016-07-21 19:38:57 +0100

    Refactor Navigation (see #51)
    - use NavigationExperimental Card Stack
    - simplify navigationState separating tabs reducer from scene reducer
    - simplify navigation views
    - handle Android back button
    - remove unnecessary import of AppRouter - fixes #85

--- src/modules/navigation/NavigationState.js
+++ src/modules/navigation/NavigationState.js
@@ -1,10 +1,13 @@
 import {fromJS} from 'immutable';
 
+import {NavigationExperimental} from 'react-native';
+
+const {StateUtils: NavigationStateUtils} = NavigationExperimental;
+
 // Actions
 const PUSH_ROUTE = 'NavigationState/PUSH_ROUTE';
 const POP_ROUTE = 'NavigationState/POP_ROUTE';
 const SWITCH_TAB = 'NavigationState/SWITCH_TAB';
-const NAVIGATION_COMPLETED = 'NavigationState/NAVIGATION_COMPLETED';
 
 export function switchTab(index) {
   return {
@@ -14,13 +17,10 @@ export function switchTab(index) {
 }
 
 // Action creators
-export function pushRoute(state) {
-  return (dispatch, getState) => {
-    // conditionally execute push to avoid double
-    // navigations due to impatient users
-    if (!isNavigationAnimationInProgress(getState())) {
-      dispatch({type: PUSH_ROUTE, payload: state});
-    }
+export function pushRoute(route) {
+  return {
+    type: PUSH_ROUTE,
+    payload: route
   };
 }
 
@@ -28,56 +28,72 @@ export function popRoute() {
   return {type: POP_ROUTE};
 }
 
-export function navigationCompleted() {
-  return {type: NAVIGATION_COMPLETED};
-}
-
-const initialState = fromJS(
-  createNavigationState('MainNavigation', 'App', [
-    createNavigationState('HomeTab', 'Home', [{key: 'Counter', title: 'Counter'}]),
-    createNavigationState('ProfileTab', 'Profile', [{key: 'Color', title: 'Color'}])
-  ]));
+// reducers for tabs and scenes are separate
+const initialState = fromJS({
+  tabs: {
+    index: 0,
+    routes: [
+      {key: 'HomeTab', title: 'HOME'},
+      {key: 'ProfileTab', title: 'PROFILE'}
+    ]
+  },
+  // Scenes for the `HomeTab` tab.
+  HomeTab: {
+    index: 0,
+    routes: [{key: 'Counter', title: 'Counter Screen'}]
+  },
+  // Scenes for the `ProfileTab` tab.
+  ProfileTab: {
+    index: 0,
+    routes: [{key: 'Color', title: 'Color Screen'}]
+  }
+});
 
 export default function NavigationReducer(state = initialState, action) {
   switch (action.type) {
-    case PUSH_ROUTE:
-      return state
-        .set('isNavigating', true)
-        .updateIn(['routes', state.get('index')], tabState =>
-          tabState
-            .update('routes', routes => routes.push(fromJS(action.payload)))
-            .set('index', tabState.get('routes').size));
+    case PUSH_ROUTE: {
+      // Push a route into the scenes stack.
+      const route = action.payload;
+      const tabs = state.get('tabs');
+      const tabKey = tabs.getIn(['routes', tabs.get('index')]).get('key');
+      const scenes = state.get(tabKey).toJS();
+      let nextScenes;
+      // fixes issue #52
+      // the try/catch block prevents throwing an error when the route's key pushed
+      // was already present. This happens when the same route is pushed more than once.
+      try {
+        nextScenes = NavigationStateUtils.push(scenes, route);
+      } catch (e) {
+        nextScenes = scenes;
+      }
+      if (scenes !== nextScenes) {
+        return state.set(tabKey, fromJS(nextScenes));
+      }
+      return state;
+    }
 
-    case POP_ROUTE:
-      return state
-        .set('isNavigating', true)
-        .updateIn(['routes', state.get('index')], tabState =>
-          tabState
-            .update('routes', routes => routes.pop())
-            .set('index', tabState.get('routes').size - 2));
+    case POP_ROUTE: {
+      // Pops a route from the scenes stack.
+      const tabs = state.get('tabs');
+      const tabKey = tabs.getIn(['routes', tabs.get('index')]).get('key');
+      const scenes = state.get(tabKey).toJS();
+      const nextScenes = NavigationStateUtils.pop(scenes);
+      if (scenes !== nextScenes) {
+        return state.set(tabKey, fromJS(nextScenes));
+      }
+      return state;
+    }
 
-    case SWITCH_TAB:
-      return state.set('index', action.payload);
-
-    case NAVIGATION_COMPLETED:
-      return state.set('isNavigating', false);
+    case SWITCH_TAB: {
+      // Switches the tab.
+      const tabs = NavigationStateUtils.jumpToIndex(state.get('tabs').toJS(), action.payload);
+      if (tabs !== state.get('tabs')) {
+        return state.set('tabs', fromJS(tabs));
+      }
+      return state;
+    }
 
     default:
       return state;
   }
-}
-
-// Helper for creating a state object compatible with the
-// RN NavigationExperimental navigator
-function createNavigationState(key, title, routes) {
-  return {
-    key,
-    title,
-    index: 0,
-    routes
-  };
-}
-
-function isNavigationAnimationInProgress(state) {
-  return state.getIn(['navigationState', 'isNavigating']);
 }
