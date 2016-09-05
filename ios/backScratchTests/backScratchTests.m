bd9db5a6cafa155f94a16232364fa2ab2fe85bef
Harvey Sanders <sanders.harvey@gmail.com>
2016-09-05 16:46:29 -0500

    (chore) change name to backScratch

--- ios/backScratchTests/backScratchTests.m
+++ ios/backScratchTests/backScratchTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface PepperoniAppTemplateTests : XCTestCase
+@interface backScratchTests : XCTestCase
 
 @end
 
-@implementation PepperoniAppTemplateTests
+@implementation backScratchTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
