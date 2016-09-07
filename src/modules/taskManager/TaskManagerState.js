050efd2006c5aad8100f0f7a1a3eea04c5f15295
Harvey Sanders <sanders.harvey@gmail.com>
2016-09-07 11:14:52 -0500

    (feat) add inital taskManager module [copy of counter]

---
+++ src/modules/taskManager/TaskManagerState.js
@@ -0,0 +1,62 @@
+import {Map} from 'immutable';
+import {loop, Effects} from 'redux-loop';
+import {generateRandomNumber} from '../../services/randomNumberService';
+
+// Initial state
+const initialState = Map({
+  value: 0,
+  loading: false
+});
+
+// Actions
+const INCREMENT = 'CounterState/INCREMENT';
+const RESET = 'CounterState/RESET';
+const RANDOM_REQUEST = 'CounterState/RANDOM_REQUEST';
+const RANDOM_RESPONSE = 'CounterState/RANDOM_RESPONSE';
+
+// Action creators
+export function increment() {
+  return {type: INCREMENT};
+}
+
+export function reset() {
+  return {type: RESET};
+}
+
+export function random() {
+  return {
+    type: RANDOM_REQUEST
+  };
+}
+
+export async function requestRandomNumber() {
+  return {
+    type: RANDOM_RESPONSE,
+    payload: await generateRandomNumber()
+  };
+}
+
+// Reducer
+export default function CounterStateReducer(state = initialState, action = {}) {
+  switch (action.type) {
+    case INCREMENT:
+      return state.update('value', value => value + 1);
+
+    case RESET:
+      return initialState;
+
+    case RANDOM_REQUEST:
+      return loop(
+        state.set('loading', true),
+        Effects.promise(requestRandomNumber)
+      );
+
+    case RANDOM_RESPONSE:
+      return state
+        .set('loading', false)
+        .set('value', action.payload);
+
+    default:
+      return state;
+  }
+}
---
+++ src/modules/taskManager/TaskManagerView.js
@@ -0,0 +1,146 @@
+import * as TaskManagerState from './TaskManagerState';
+import * as NavigationState from '../../modules/navigation/NavigationState';
+import React, {PropTypes} from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Text,
+  View
+} from 'react-native';
+
+const CounterView = React.createClass({
+  propTypes: {
+    counter: PropTypes.number.isRequired,
+    userName: PropTypes.string,
+    userProfilePhoto: PropTypes.string,
+    loading: PropTypes.bool.isRequired,
+    dispatch: PropTypes.func.isRequired
+  },
+  increment() {
+    this.props.dispatch(TaskManagerState.increment());
+  },
+  reset() {
+    this.props.dispatch(TaskManagerState.reset());
+  },
+  random() {
+    this.props.dispatch(TaskManagerState.random());
+  },
+  bored() {
+    this.props.dispatch(NavigationState.pushRoute({
+      key: 'Color',
+      title: 'Color Screen'
+    }));
+  },
+
+  renderUserInfo() {
+    if (!this.props.userName) {
+      return null;
+    }
+
+    return (
+      <View style={styles.userContainer}>
+        <Image
+          style={styles.userProfilePhoto}
+          source={{
+            uri: this.props.userProfilePhoto,
+            width: 80,
+            height: 80
+          }}
+        />
+        <Text style={styles.linkButton}>
+          Welcome, {this.props.userName}!
+        </Text>
+      </View>
+    );
+  },
+  render() {
+    const loadingStyle = this.props.loading
+      ? {backgroundColor: '#eee'}
+      : null;
+
+    return (
+      <View style={styles.container}>
+
+        {this.renderUserInfo()}
+
+        <TouchableOpacity
+          onPress={this.increment}
+          style={[styles.counterButton, loadingStyle]}>
+          <Text style={styles.counter}>
+            {this.props.counter}
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.reset}>
+          <Text style={styles.linkButton}>
+            Reset
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.random}>
+          <Text style={styles.linkButton}>
+            Random
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.bored} accessible={true}>
+          <Text style={styles.linkButton}>
+            {'I\'m bored!'}
+          </Text>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+});
+
+const circle = {
+  borderWidth: 0,
+  borderRadius: 40,
+  width: 80,
+  height: 80
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  userContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  userProfilePhoto: {
+    ...circle,
+    alignSelf: 'center'
+  },
+  counterButton: {
+    ...circle,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 20
+  },
+  counter: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  welcome: {
+    textAlign: 'center',
+    color: 'black',
+    marginBottom: 5,
+    padding: 5
+  },
+  linkButton: {
+    textAlign: 'center',
+    color: '#CCCCCC',
+    marginBottom: 10,
+    padding: 5
+  }
+});
+
+export default CounterView;
