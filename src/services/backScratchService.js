e711d08f52acc9e6dc63a989b7309b6bad02f032
Harvey Sanders <sanders.harvey@gmail.com>
2016-09-08 18:52:31 -0500

    (feat) add new API routes

--- src/services/backScratchService.js
+++ src/services/backScratchService.js
@@ -1,14 +1,24 @@
 import * as api from '../utils/api';
 
+// ------------- Tasks ------------
 export async function getTasks() {
   // return GET promise
   return api.get('/tasks');
+}
+
+export async function getRequestedTasks(id) {
+  return api.get(`/tasks/${id}`);
 }
 
 export async function postTask(task) {
   return api.post('/tasks', task);
 }
 
+export async function updateTask(id, newProps) {
+  return api.put(`/tasks/${id}`, newProps);
+}
+
+// ------------- Users ------------
 export async function getUserInfo(id) {
   return api.get(`/users/${id}`);
 }
@@ -19,4 +29,8 @@ export async function loginUser(email) {
 
 export async function createUser(user) {
   return api.post('/users', user);
+}
+
+export async function updateUser(id, newProps) {
+  return api.put(`/users/${id}`, newProps);
 }
