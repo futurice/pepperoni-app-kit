9b6bbae8a649f5e8d1f0b5a2360ed119b5ac54e2
Daniel Glaser <glaserd92@gmail.com>
2016-09-08 16:26:46 -0500

    (scaffold) Add styles file for future global styles

---
+++ src/styles/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  darkPrimaryColor: '#388E3C',
+  primaryColor: '#4CAF50',
+  lightPrimaryColor: '#C8E6C9',
+  accent: '#536DFE',
+  white: '#FFFFFF',
+  black: '#212121',
+  grey: '#757575',
+  divider: '#BDBDBD'
+};
+
+export default colors;
