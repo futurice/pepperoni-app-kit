d45465210cc706c34c2fee92f3436f5e45ce07df
Neal Taylor, Jr <neal.taylorjr@gmail.com>
2016-09-08 15:52:35 -0500

    (feat) build base task form module -need styling-

--- src/modules/taskManager/taskForm/TaskFormView.js
+++ src/modules/taskManager/taskForm/TaskFormView.js
@@ -1,11 +1,16 @@
+import Calendar from 'react-native-calendar';
 import React, {PropTypes} from 'react';
 import {
-  Text,
-  View,
-  StyleSheet
+  ScrollView
 } from 'react-native';
+import {
+  Form,
+  Separator,
+  InputField,
+  PickerField
+} from 'react-native-form-generator';
 
-import * as NavigationState from '../../../modules/navigation/NavigationState';
+// import * as NavigationState from '../../../modules/navigation/NavigationState';
 
 /**
  * Sample view to demonstrate navigation patterns.
@@ -25,20 +30,52 @@ const TaskFormView = React.createClass({
   render() {
 
     return (
-      <View style={[styles.container, {backgroundColor: this.state.background}]}>
-        <Text onPress={this.onNextPress}>
-          Hey!
-        </Text>
-      </View>
+      <ScrollView>
+        <Form ref='taskForm'
+          onFocus={this.handleFormFocus}
+          onChange={this.handleFormChange}
+        >
+          <InputField
+            ref='taskName'
+            placeholder='Task Name'
+            helpText='Give your task a short but informative name, like Ex: Need help mowing my lawn.'
+          />
+          <InputField
+            multiline={true}
+            ref='desc'
+            placeholder='Task Description'
+            helpText={'Add a short descripition of what the task will entail. Be straightforward so other users can see if they can help!'}
+          />
+          <PickerField
+            ref='type'
+            label='Task Category'
+            options={{
+              '': '',
+              handyman: 'Handyman',
+              errands: 'Errands',
+              domestic: 'Domestic',
+              physicalLabor: 'Physical Labor',
+              informative: 'Informative',
+              misc: 'Miscellaneous'
+            }}
+            helpText='Pick the Category that your task best falls under.'
+          />
+          <Separator />
+          <InputField
+            multiline={true}
+            ref='address'
+            placeholder='Task Address'
+            helpText='Tell us where the task will done.'
+          />
+          <Calendar
+          scrollEnabled={true}
+          dayHeadings={['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']}
+          weekStart={0}
+          customStyle={{currentDayCircle: {backgroundColor: '#388E3C'}}}
+         />
+        </Form>
+      </ScrollView>
     );
-  }
-});
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
   }
 });
 
