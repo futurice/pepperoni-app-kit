fa719d763d8de1674aa8fef6da1381112f45eda8
Harvey Sanders <sanders.harvey@gmail.com>
2016-09-10 08:09:36 -0500

    (feat) calling loginUser on after successful Auth0 login to grab userInfo fromAPI server -- not yet working fully

--- src/modules/user/UserState.js
+++ src/modules/user/UserState.js
@@ -1,43 +1,59 @@
-import {Map} from 'immutable';
+import {Map, fromJS} from 'immutable';
 import {loop, Effects} from 'redux-loop';
-import {getUserTasks} from '../../services/backScratchService';
+import {loginUser} from '../../services/backScratchService';
 
 // Initial state
 const initialState = Map({
-  loading: false
+  loading: false,
+  value: {}
 });
 
 // Actions
-const RANDOM_REQUEST = 'CounterState/RANDOM_REQUEST';
-const RANDOM_RESPONSE = 'CounterState/RANDOM_RESPONSE';
+const LOGIN = 'UserState/LOGIN';
+const LOGIN_RESPONSE = 'UserState/LOGIN_RESPONSE';
 
 // Action creators
-export function random() {
+export function login(email) {
   return {
-    type: RANDOM_REQUEST
+    type: LOGIN,
+    payload: email
   };
 }
 
-export async function requestUserTasks() {
+// export function userTasks(userId) {
+//   return {
+//     type: RANDOM_REQUEST,
+//     payload: userId
+//   };
+// }
+
+export async function requestUserLogin(email) {
   return {
-    type: RANDOM_RESPONSE,
-    payload: await getUserTasks()
+    type: LOGIN_RESPONSE,
+    payload: await loginUser(email)
   };
 }
+
+// export async function requestUserTasks() {
+//   return {
+//     type: USER_TASKS_RESPONSE,
+//     payload: await getUserTasks()
+//   };
+// }
 
 // Reducer
 export default function UserStateReducer(state = initialState, action = {}) {
   switch (action.type) {
-    case RANDOM_REQUEST:
+    case LOGIN:
       return loop(
         state.set('loading', true),
-        Effects.promise(getUserTasks)
+        Effects.promise(requestUserLogin, action.payload)
       );
 
-    case RANDOM_RESPONSE:
+    case LOGIN_RESPONSE:
       return state
         .set('loading', false)
-        .set('value', action.payload);
+        .set('value', fromJS(action.payload));
 
     default:
       return state;
