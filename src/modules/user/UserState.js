7599a616613b807775380a2ea7c0238209170ed5
Harvey Sanders <sanders.harvey@gmail.com>
2016-09-10 09:54:39 -0500

    (chore) code clean up. remove uneeeded import

--- src/modules/user/UserState.js
+++ src/modules/user/UserState.js
@@ -1,6 +1,5 @@
 import {Map, fromJS} from 'immutable';
 import {loop, Effects} from 'redux-loop';
-import {loginUser} from '../../services/backScratchService';
 
 // Initial state
 const initialState = Map({
@@ -12,27 +11,12 @@ const initialState = Map({
 const USER_FOUND = 'UserState/USER_FOUND';
 
 // Action creators
-
-// export function userTasks(userId) {
-//   return {
-//     type: RANDOM_REQUEST,
-//     payload: userId
-//   };
-// }
-
 export function onExistingUser(user) {
   return {
     type: USER_FOUND,
     payload: fromJS(user)
   };
 }
-
-// export async function requestUserTasks() {
-//   return {
-//     type: USER_TASKS_RESPONSE,
-//     payload: await getUserTasks()
-//   };
-// }
 
 // Reducer
 export default function UserStateReducer(state = initialState, action = {}) {
