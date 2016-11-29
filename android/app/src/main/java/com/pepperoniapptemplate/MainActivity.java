b4e447d6ccecae3cdc225414fda311dd09c4ecfa
Lauri Lavanti <lauri.lavanti@futurice.com>
2016-11-29 18:59:01 +0200

    Add react-native-vector-icons and example icons into TabBarButton.js

--- android/app/src/main/java/com/pepperoniapptemplate/MainActivity.java
+++ android/app/src/main/java/com/pepperoniapptemplate/MainActivity.java
@@ -1,6 +1,7 @@
 package com.pepperoniapptemplate;
 
 import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainActivity extends ReactActivity {
 
