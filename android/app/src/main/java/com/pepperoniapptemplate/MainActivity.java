80f4af7fc8efe098b5c358ca4988ca2e85fb5577
Lauri Lavanti <lauri.lavanti@futurice.com>
2016-12-04 18:53:18 +0200

    Do requested changes to Podfile and MainActivity/MainApplication

--- android/app/src/main/java/com/pepperoniapptemplate/MainActivity.java
+++ android/app/src/main/java/com/pepperoniapptemplate/MainActivity.java
@@ -1,7 +1,6 @@
 package com.pepperoniapptemplate;
 
 import com.facebook.react.ReactActivity;
-import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainActivity extends ReactActivity {
 
--- android/app/src/main/java/com/pepperoniapptemplate/MainApplication.java
+++ android/app/src/main/java/com/pepperoniapptemplate/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-        new MainReactPackage()
+        new MainReactPackage(),
+        new VectorIconsPackage()
       );
     }
   };
