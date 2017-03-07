9162228e1630bd4c250bb59257a722f15a2343bb
Rasmus Eskola <fruitiex@gmail.com>
2017-03-07 11:59:42 +0200

    Fix tests

---
+++ test/setup.js
@@ -0,0 +1,8 @@
+// https://github.com/facebook/react-native/issues/11585
+jest.mock('Linking', () => ({
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  openURL: jest.fn(),
+  canOpenURL: jest.fn(),
+  getInitialURL: jest.fn()
+}));
