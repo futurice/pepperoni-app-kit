arthur31416/pepperoni

Rename counter module to location

src/redux/reducer.js
@@ -2,15 +2,15 @@ import {Map} from 'immutable';
 import {combineReducers} from 'redux-loop';
 import NavigationStateReducer from '../modules/navigation/NavigationState';
 import AuthStateReducer from '../modules/auth/AuthState';
-import CounterStateReducer from '../modules/counter/CounterState';
+import LocationStateReducer from '../modules/location/LocationState';
 import SessionStateReducer, {RESET_STATE} from '../modules/session/SessionState';
 
 const reducers = {
   // Authentication/login state
   auth: AuthStateReducer,
 
-  // Counter sample app state. This can be removed in a live application
-  counter: CounterStateReducer,
+  // Location app state
+  location: LocationStateReducer,
 
   // @NOTE: By convention, the navigation state must live in a subtree called
   //`navigationState`

futurice/pepperoni-app-kit

src/redux/reducer.js
@@ -1,12 +1,12 @@
 import {Map, fromJS} from 'immutable';
 import {loop, combineReducers} from 'redux-loop';
 import NavigationStateReducer from '../modules/navigation/NavigationState';
-import CounterStateReducer from '../modules/counter/CounterState';
+import CounterStateReducer from '../modules/location/LocationState';
 import SessionStateReducer, {RESET_STATE} from '../modules/session/SessionState';
 
 const reducers = {
-  // Counter sample app state. This can be removed in a live application
-  counter: CounterStateReducer,
+  // Location app state
+  location: LocationStateReducer,
 
   // @NOTE: By convention, the navigation state must live in a subtree called
   //`navigationState`