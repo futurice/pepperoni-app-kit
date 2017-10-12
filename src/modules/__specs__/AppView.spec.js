c2ac7673769b54c6cd4bca390400864425cbb928
Daniel J Harvey <dan.harvey@futurice.com>
2017-10-12 15:16:36 +0100

    Fix enzyme tests

--- src/modules/__specs__/AppView.spec.js
+++ src/modules/__specs__/AppView.spec.js
@@ -1,7 +1,14 @@
 /*eslint-disable max-nested-callbacks*/
 
 import React from 'react';
-import {shallow} from 'enzyme';
+
+import { shallow, configure } from 'enzyme';
+
+// fix Enzyme to work with React 16 as per https://github.com/airbnb/enzyme#installation
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 import {ActivityIndicator} from 'react-native';
 import AppView from '../AppView';
 
